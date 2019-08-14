bde0c84a8a0e1da2cdd79a88300646dbe4acdad5
Daniel Koska <daniel.koska@hsw.tu-chemnitz.de>
2019-08-14 10:35:27 +0200

    test

---
+++ man/hello_fab.R
@@ -0,0 +1,13 @@
+#' @title Test
+#'
+#' @description Test
+#'
+#' @param symbol
+#'
+#' @return NULL
+#'
+#' @export
+
+hello_fab <- function() {
+  print("Hello, world ... Das ist das erste Testpaket der FAB!")
+}
